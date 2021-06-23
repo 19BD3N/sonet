fac3544b80ae7afa378ebc1e001acbf364a31ef9
jayasimha reddy <jayasimhareddy27@gmail.com>
2021-06-23 20:54:45 +0530

    lite server done

---
+++ ajaxjs.js
@@ -0,0 +1,97 @@
+display=()=>{
+    var mylist=document.getElementById("myList");
+    var city=mylist.options[mylist.selectedIndex].text;
+    var request = new XMLHttpRequest();                     // create a request
+    request.open("GET","https://api.openweathermap.org/data/2.5/weather?q=" +city +"&units=metric&appid=93f26e3c57081a6210de53b8dcfdfea4",true);
+    request.onload =  function(){
+            
+        if( request.status>=200 && request.status<400){
+            var data = JSON.parse(request.responseText);
+            getdetails(data);
+        }
+        else {
+            console.log("some error");
+        }
+    };
+    request.onerror = function(){
+            console.log("connection error");
+    };
+    request.send();
+};
+
+getdetails=(data)=>{
+    console.log(data);
+    document.getElementById('temp').innerHTML = data.main.temp;
+    document.getElementById('weather').innerHTML = data.weather[0].description;
+    var icon = data.weather[0].icon+".png";
+    document.getElementById('myimg').src = "https://openweathermap.org/img/w/"+icon ;
+};
+
+
+/*      
+function display(){
+
+    var mylist = document.getElementById("myList");  
+
+    switch(mylist.options[mylist.selectedIndex].text){
+        
+        case "warangal": {console.log("yes!!");
+                          warangal();         
+                           break;
+                          }
+
+        case "Hyderabad": {console.log("yes!!");
+                            Hyderabad();
+                           break;
+                          } 
+
+        default : document.getElementById('para').innerHTML = "";
+                          break;
+    }
+}
+
+
+function Hyderabad(){
+    request.open("GET","https://api.openweathermap.org/data/2.5/weather?q=Hyderabad&appid=93f26e3c57081a6210de53b8dcfdfea4",true);
+
+    request.onload =  function(){
+        
+        if( request.status>=200 && request.status<400){
+            var data = JSON.parse(request.responseText);
+            finalshot(data);
+        }
+        else {
+            console.log("some error");
+        }
+    };
+    request.onerror = function(){
+            console.log("connection error");
+    };
+request.send();
+}     
+
+
+function warangal(){
+request.open("GET","https://api.openweathermap.org/data/2.5/weather?q=Warangal&appid=93f26e3c57081a6210de53b8dcfdfea4",true);
+    request.onload =  function(){
+        if( request.status>=200 && request.status<400){
+            var data = JSON.parse(request.responseText);
+            finalshot(data);
+        }
+        else {
+            console.log("some error");
+        }
+    };
+    request.onerror = function(){
+            console.log("connection error");
+    };
+request.send();
+}
+
+function finalshot(data){
+    document.getElementById('temp').innerHTML = data.main.temp;
+    document.getElementById('weather').innerHTML = data.weather[0].description;
+    var icon = data.weather[0].icon+".png";
+    document.getElementById('myimg').src = "https://openweathermap.org/img/w/"+icon ;
+}
+*/
